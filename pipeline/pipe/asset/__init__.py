44efd3de43ebc8c44baf2f060a046dbfe55a860e
Joseph Matthew Wardle <joseward@byu.edu>
2026-03-05 22:09:16 -0700

    Add high-level asset version service helpers

--- pipeline/pipe/asset/__init__.py
+++ pipeline/pipe/asset/__init__.py
@@ -1,6 +1,7 @@
 """Shared asset pipeline helpers."""
 
 from .paths import AssetPaths, asset_root, asset_root_from_path, paths_for_asset
+from .version_service import list_version_records, promote_version, save_version
 from .versioning import (
     backup_file,
     get_manifest_path,
@@ -14,6 +15,9 @@ __all__ = [
     "asset_root",
     "asset_root_from_path",
     "paths_for_asset",
+    "save_version",
+    "promote_version",
+    "list_version_records",
     "backup_file",
     "get_manifest_path",
     "load_manifest",
